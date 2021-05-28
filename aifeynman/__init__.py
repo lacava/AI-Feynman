d53c055ca17d0684fd7ba7d72df8856668e2e132
lacava <williamlacava@gmail.com>
2021-05-28 00:44:30 -0400

    version bump

--- aifeynman/__init__.py
+++ aifeynman/__init__.py
@@ -6,7 +6,7 @@ from .S_run_aifeynman import run_aifeynman
 from .get_demos import get_demos
 
 __title__ = "aifeynman"
-__version__ = "2.0.7.5"
+__version__ = "2.0.7.6"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Silviu-Marian Udrescu"
 
